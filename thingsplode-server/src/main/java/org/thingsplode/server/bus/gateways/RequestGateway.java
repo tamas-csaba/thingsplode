ca0e90e47c97e228a65230bfc75fbc0828f3bb18
CSaba.Tamas <tamas.csaba@gmail.com>
2015-03-08 10:37:38 +0100

    Improving the domain model

---
+++ thingsplode-server/src/main/java/org/thingsplode/server/bus/gateways/RequestGateway.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.thingsplode.server.bus.gateways;
+
+
+import java.util.concurrent.Future;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.Payload;
+import org.thingsplode.core.protocol.AbstractRequest;
+import org.thingsplode.core.protocol.Response;
+
+/**
+ *
+ * @author tamas.csaba@gmail.com
+ */
+@MessagingGateway(name = "requestEntryGateway", defaultRequestChannel = "requestChannel")
+public interface RequestGateway {
+    
+    public Future<Response> execute(@Payload AbstractRequest request);    
+    
+}
---
+++ thingsplode-server/src/main/java/org/thingsplode/server/bus/gateways/SyncGateway.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.thingsplode.server.bus.gateways;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.Payload;
+import org.thingsplode.core.protocol.AbstractRequest;
+
+/**
+ *
+ * @author tamas.csaba@gmail.com
+ */
+@MessagingGateway(name = "syncEntryGateway", defaultRequestChannel = "syncChannel")
+public interface SyncGateway {
+    
+    public void process(@Payload AbstractRequest sync);
+}
