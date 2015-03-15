ab5704b395d40c321b2e0a6a182f39a07d3f4ebb
CSaba.Tamas <tamas.csaba@gmail.com>
2015-03-15 17:37:14 +0100

    fixing fixing fixing

--- thingsplode-core/src/main/java/org/thingsplode/core/protocol/response/ConfigurationCarrierResponse.java
+++ thingsplode-core/src/main/java/org/thingsplode/core/protocol/response/ConfigurationCarrierResponse.java
@@ -5,10 +5,11 @@
  */
 package org.thingsplode.core.protocol.response;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashMap;
+import org.thingsplode.core.entities.Component;
 import org.thingsplode.core.entities.Configuration;
+import org.thingsplode.core.entities.Device;
 import org.thingsplode.core.protocol.ExecutionStatus;
 import org.thingsplode.core.protocol.Response;
 import org.thingsplode.core.protocol.ResponseCode;
@@ -20,7 +21,7 @@ import org.thingsplode.core.protocol.ResponseCode;
  */
 public abstract class ConfigurationCarrierResponse<T extends ConfigurationCarrierResponse<T>> extends Response<T> {
 
-    private Collection<Configuration> configuration;
+    private HashMap<String, Collection<Configuration>> configuration; //component name / configurations
 
     public ConfigurationCarrierResponse() {
     }
@@ -36,30 +37,43 @@ public abstract class ConfigurationCarrierResponse<T extends ConfigurationCarrie
     public ConfigurationCarrierResponse(String responseCorrelationID, ExecutionStatus requestStatus, ResponseCode responseCode, String resultMessage) {
         super(responseCorrelationID, requestStatus, responseCode, resultMessage);
     }
-    
-    /**
-     * @return the configuration
-     */
-    public Collection<Configuration> getConfiguration() {
+
+    public HashMap<String, Collection<Configuration>> getConfiguration() {
         return configuration;
     }
 
-    /**
-     * @param configuration the configuration to set
-     */
-    public void setConfiguration(Collection<Configuration> configuration) {
+    public void setConfiguration(HashMap<String, Collection<Configuration>> configuration) {
         this.configuration = configuration;
     }
 
-    public T addConfigurations(Configuration... cfgs) {
+    public T addConfigurationsFromDevice(Device d) {
+        if (d != null) {
+            initializeConfiguration();
+            addConfigurationFromComponent(d);
+        }
+        return (T) this;
+    }
+
+    private void addConfigurationFromComponent(Component<?> comp) {
+        if (comp != null) {
+            if (comp.getConfiguration() != null && !comp.getConfiguration().isEmpty()) {
+                this.configuration.put(comp.getName(), comp.getConfiguration());
+            }
+            if (comp.getComponents() != null && !comp.getComponents().isEmpty()) {
+                comp.getComponents().forEach(sc -> addConfigurationFromComponent(sc));
+            }
+        }
+    }
+
+    public T addConfigurations(HashMap<String, Collection<Configuration>> cfgs) {
         initializeConfiguration();
-        Collections.addAll(this.getConfiguration(), cfgs);
-        return (T)this;
+        this.getConfiguration().putAll(cfgs);
+        return (T) this;
     }
 
     private void initializeConfiguration() {
         if (configuration == null) {
-            this.configuration = new ArrayList<>();
+            this.configuration = new HashMap<>();
         }
     }
 
